e2bce6ccefb7d20b5a646414dcbfaf1470482200
EricCartman01 <leomonte01@gmail.com>
2021-04-29 10:22:54 -0300

    CRUD Local Done

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, List } from 'semantic-ui-react';
+import { Grid} from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import ActivityDetails from '../details/ActivityDetails';
 import AcitivityForm from '../form/ActivityForm';
@@ -13,20 +13,22 @@ interface Props{
     editMode: boolean;
     openForm: (id: string) => void;
     closeForm: () => void; 
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
 
 //Pode usar Destructure com activities : Props
-export default function ActivityDashboard({activities, selectActivity, selectedActivity, cancelSelectActivity, editMode, openForm, closeForm}: Props){
+export default function ActivityDashboard({activities, selectActivity, selectedActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props){
     return(
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} selectActivity={selectActivity}/>
+                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}/>
             </Grid.Column>
             <Grid.Column width='6'>
-                {selectedActivity &&
+                {selectedActivity && !editMode &&
                 <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity} openForm={openForm}/>}
                 {editMode &&
-                <AcitivityForm closeForm={closeForm} activity={selectedActivity} />}
+                <AcitivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
             </Grid.Column>
         </Grid>
     )
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,22 +1,46 @@
-import React from 'react'
-import { Button, Form, Grid, Segment } from 'semantic-ui-react'
+import React, { ChangeEvent, useState } from 'react'
+import { Button, Form, Segment } from 'semantic-ui-react'
 import { Activity } from '../../../app/models/activity'
 
 interface Props{
     activity: Activity | undefined;
     closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
 }
 
-export default function AcitivityForm({activity, closeForm}:Props){
+export default function AcitivityForm({activity, closeForm, createOrEdit}:Props){
+
+    const initialState = activity ?? {
+        id: '',
+        title: '',
+        category: '',
+        description: '',
+        date: '',
+        city: '',
+        venue: ''
+    }
+
+    const [_activity,setActivity] = useState(initialState);
+
+    function handleSubmit(){
+        createOrEdit(_activity);
+    }
+
+    function handleInputChage(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
+        const {name, value} = event.target;
+        setActivity({..._activity,[name]:value});
+    }
+
+
     return(
         <Segment clearing>
-            <Form>
-                <Form.Input placeholder='Title'/>
-                <Form.TextArea placeholder='Description'/>
-                <Form.Input placeholder='Category'/>
-                <Form.Input placeholder='Date'/>
-                <Form.Input placeholder='City'/>
-                <Form.Input placeholder='Venue'/>
+            <Form onSubmit={handleSubmit} autoComplete='off'>
+                <Form.Input placeholder='Title' value={_activity.title} name='title' onChange={handleInputChage}/>
+                <Form.TextArea placeholder='Description' value={_activity.description} name='description' onChange={handleInputChage}/>
+                <Form.Input placeholder='Category' value={_activity.category} name='category' onChange={handleInputChage}/>
+                <Form.Input placeholder='Date' value={_activity.date} name='date' onChange={handleInputChage}/>
+                <Form.Input placeholder='City' value={_activity.city} name='city' onChange={handleInputChage}/>
+                <Form.Input placeholder='Venue' value={_activity.venue} name='venue' onChange={handleInputChage}/>
                 <Button floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel'/>
             </Form>
