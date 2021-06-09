9696c91b988f0a7d577f50f4f925ffb9e0521409
EricCartman01 <leomonte01@gmail.com>
2021-06-09 08:30:49 -0300

    Router

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,7 +1,6 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
-import {v4 as uuid} from 'uuid';
 
 export default class ActivityStore{
 
@@ -17,16 +16,16 @@ export default class ActivityStore{
     }
 
     get ActivitiesByDate (){
-        return Array.from(this.activities.values()).sort((a,b)=>Date.parse(a.date) - Date.parse(b.date));
+        //return Array.from(this.activities.values()).sort((a,b)=>Date.parse(a.date) - Date.parse(b.date));
+        return Array.from(this.activityRegistry.values()).sort((a,b) => Date.parse(a.date) - Date.parse(b.date));
     }
 
     loadActivities = async () => {
+        this.loadingInitial = true;
         try{
             const activities = await agent.Activities.list();
             activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                //this.activityRegistry.set(activity.id, activity);
-                this.activities.push(activity);
+                this.setActivity(activity);
             })
             this.setLoadingInitial(false);
         }catch(error){
@@ -35,31 +34,49 @@ export default class ActivityStore{
         }
     }
 
+    loadActivity = async (id: string) => {
+        let activity = this.getActivity(id);
+        console.log('activity id: ' + activity?.id);
+
+        if(activity){
+            this.selectedActivity = activity;
+            return activity;
+        }
+        else{
+            this.loadingInitial = true;
+            try{
+                activity = await agent.Activities.details(id);
+                this.setActivity(activity);
+                runInAction(()=>{
+                    this.selectedActivity = activity;
+                })
+                this.setLoadingInitial(false);
+                return activity;
+            }
+            catch (error){
+                console.log(error);
+                this.setLoadingInitial(false);
+            }
+        }
+    }
+
+    private setActivity = (activity: Activity) =>{
+        activity.date = activity.date.split('T')[0];
+        this.activityRegistry.set(activity.id, activity);
+        //this.activities.push(activity);
+    }
+
+    private getActivity = (id: string) =>{
+        return this.activityRegistry.get(id);
+        //return this.activities.find(x => x.id === id);
+    }
+
     setLoadingInitial = (state: boolean) =>{
         this.loadingInitial = state;
     }
-
-    selectActivity = (id: string) => {
-        this.selectedActivity = this.activities.find(x => x.id === id);
-        //this.selectedActivity = this.activityRegistry.get(id);
-    }
-
-    cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    }
-
-    openForm = (id?: string) => {
-        id ? this.selectActivity(id) : this.cancelSelectedActivity();
-        this.editMode = true;
-    }
-
-    closeForm = () => {
-        this.editMode = false;
-    }
-
+   
     createActivity = async (activity: Activity) => {
         this.loading = true;
-        activity.id = uuid();
         try{
             await agent.Activities.create(activity);
             runInAction(()=>{
@@ -102,7 +119,9 @@ export default class ActivityStore{
             await agent.Activities.delete(id);
             runInAction(()=>{
                 this.activities = [...this.activities.filter(a => a.id !== id)];
-                if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
+                //this.activityRegistry.delete(id);
+                //*** Code for SPA */
+                //if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
                 this.loading = false;
             })
         }catch(error){
@@ -112,5 +131,25 @@ export default class ActivityStore{
             })
         }
     }
+
+     // Metodos q poderiam ser removidos *** servem apenas ao SPA
+    selectActivity = (id: string) => {
+        //this.selectedActivity = this.activities.find(x => x.id === id);
+        this.selectedActivity = this.activityRegistry.get(id);
+    }
+
+    cancelSelectedActivity = () => {
+        this.selectedActivity = undefined;
+    }
+
+    openForm = (id?: string) => {
+        id ? this.selectActivity(id) : this.cancelSelectedActivity();
+        this.editMode = true;
+    }
+
+    closeForm = () => {
+        this.editMode = false;
+    }
+    // -------------------------
 
 }
